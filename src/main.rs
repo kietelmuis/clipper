69f3ce1b57463bff38d1fe4c99868cd369bcdd10
omar <omar.hopman9@gmail.com>
2025-08-04 22:51:56 +0200

    feat: tauri

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 mod capture;
 mod config;
 
@@ -6,9 +8,13 @@ use capture::*;
 fn main() {
     let config = config::Config::new();
 
-    capture::CaptureMuxer::new(CaptureSettings {
-        resolution: config.resolution,
-        fps: config.fps,
-    })
-    .start_muxer();
+    std::thread::spawn(move || {
+        capture::CaptureMuxer::new(CaptureSettings {
+            resolution: config.resolution,
+            fps: config.fps,
+        })
+        .start_muxer();
+    });
+
+    tauri_app_lib::run();
 }
