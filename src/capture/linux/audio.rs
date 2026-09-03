309a4a6cc3649003e466e8095d9a575d58200f4e
kietelmuis <omar.hopman9@gmail.com>
2026-09-03 22:17:16 +0200

    linux base

--- src/capture/linux/audio.rs
+++ src/capture/linux/audio.rs
@@ -1,7 +1,12 @@
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::time::{Duration, Instant};
 
 use crossbeam::channel::{self, Receiver, SendError};
+use pipewire::spa;
+use pipewire::spa::sys::SPA_DIRECTION_INPUT;
+use pipewire::spa::utils::Direction;
+use pipewire::stream::StreamFlags;
+use pipewire::sys::{PW_KEY_MEDIA_CATEGORY, PW_KEY_MEDIA_ROLE, PW_KEY_MEDIA_TYPE};
 
 #[derive(Debug)]
 pub struct AudioBuffer {
@@ -9,31 +14,31 @@ pub struct AudioBuffer {
     pub time: Duration,
 }
 
-// handle all pointers because we are gonna give the external to the muxer
-struct InternalCaptureApi {}
-
-// required (to send), but since inner is private the unsafe pointers inside wont be accessed
-unsafe impl Send for InternalCaptureApi {}
-unsafe impl Sync for InternalCaptureApi {}
-
 // this is going to the muxer
 pub struct AudioCaptureApi {
     pub audio_rx: Receiver<AudioBuffer>,
     pub sample_rate: Option<u16>,
     pub channels: Option<u32>,
+    pub bit_rate: Option<usize>,
 
-    inner: Arc<Mutex<InternalCaptureApi>>,
+    start: Arc<Instant>,
     stop_tx: channel::Sender<bool>,
 }
 
-impl Drop for InternalCaptureApi {
-    fn drop(&mut self) {
-        println!("cleaning audio api");
-    }
-}
-
 impl AudioCaptureApi {
-    pub fn new(instant: Arc<Instant>) -> Self {}
+    pub fn new(start: Arc<Instant>) -> AudioCaptureApi {
+        let (audio_tx, audio_rx) = channel::unbounded::<AudioBuffer>();
+        let (stop_tx, stop_rx) = channel::unbounded::<bool>();
+
+        Self {
+            stop_tx,
+            audio_rx,
+            start,
+            sample_rate: None,
+            channels: None,
+            bit_rate: None,
+        }
+    }
 
     pub fn start(&mut self) -> Result<(), SendError<bool>> {
         self.stop_tx.send(true)?;
@@ -45,9 +50,26 @@ impl AudioCaptureApi {
         Ok(())
     }
 
-    fn init(&mut self) {}
-}
+    fn init(&mut self) {
+        pipewire::init();
 
-impl InternalCaptureApi {
-    fn event_loop(&mut self) {}
+        let mainloop = pipewire::main_loop::MainLoop::new(None).unwrap();
+        let context = pipewire::context::Context::new(&mainloop).unwrap();
+        let core = context.connect(None).unwrap();
+        let registry = core.get_registry().unwrap();
+
+        let mut properties = pipewire::properties::Properties::new();
+        properties.insert(PW_KEY_MEDIA_TYPE, "Video");
+        properties.insert(PW_KEY_MEDIA_CATEGORY, "Capture");
+        properties.insert(PW_KEY_MEDIA_ROLE, "Camer");
+
+        let stream = pipewire::stream::Stream::new(&core, "capturer", properties).unwrap();
+
+        let flags = StreamFlags::empty();
+        flags.insert(StreamFlags::AUTOCONNECT);
+        flags.insert(StreamFlags::MAP_BUFFERS);
+
+        let params = spa::pod::builder::Builder::new(&mut Vec::new());
+        stream.connect(Direction::Input, None, flags, None).unwrap();
+    }
 }
